185779cd25ae390bff48f002c54c04ed0c19da2d
Minh Hieu <1911505310121@sv.ute.udn.vn>
2022-09-24 21:07:53 +0700

    bt tuan 2

--- classSV.rb
+++ classSV.rb
@@ -1,74 +1,52 @@
-class Students
-    def initialize(name, id, address, math, physics, chemistry)
-        @name = name
-        @id = id
-        @address = address
-        @math = math
-        @physics = physics
-        @chemistry = chemistry
+class Man 
+    attr_accessor :name, :hair, :weight, :height, :age, :phone, :email, :nation
+    
+    def initialize(name, hair, weight, height, age, phone, email, nation)
+      @name = name
+      @hair = hair
+      @weight = weight
+      @height = height
+      @age = age
+      @phone = phone
+      @email = email
+      @nation = nation
     end
-    def getName
-        return @name
-    end
-    def setName(name)
-        @name = name
-    end
-    def getId
-        return @id
-    end
-    def setId(id)
-        @id = id
-    end
-    def getAddress
-        return @address
-    end
-    def setAddress(address)
-        @address = address
-    end
-    def getMath
-        return @math
-    end
-    def setMath(math)
-        @math = math
-    end
-    def getPhysics
-        return @physics
-    end
-    def setPhysics(physics)
-        @physics = physics
-    end
-    def getChemistry
-        return @chemistry
-    end
-    def setChemistry(chemistry)
-        @chemistry = chemistry
-    end
-    def inputPoint
-        print "Math = "
-        @math = gets.chomp.to_i
-        print "Physics = "
-        @physics = gets.chomp.to_i
-        print "Chemistry = "
-        @chemistry = gets.chomp.to_i
+    
+  end
 
-    end
-    def getAvg
-        return (@math + @physics + @chemistry)/3.to_f
-    end
-    def printInfo
-        puts "Name: #{getName}"
-        puts "Id: #{getId}"
-        puts "Addess: #{getAddress}"
-        puts "Avg: #{getAvg}"
-    end
-    private :getName, :getId, :getAddress, :getMath, :getPhysics, :getChemistry
-    public :getAvg, :inputPoint, :printInfo
+
+
+class Student < Man
+  attr_accessor :grade1, :grade2, :grade3
+  def initialize(grade1, grade2, grade3,name, hair, weight, height, age, phone, email, nation)
+                               
+    @grade1 = grade1
+    @grade2 = grade2
+    @grade3 = grade3
+    super(name, hair, weight, height, age, phone, email, nation)
+
+  end
+  
+  def avg 
+    return (1.0*@grade1 + @grade2+ @grade3)/3
+  end
+
+  def to_s
+    return "#{@name} | #{@hair} | #{@weight} | #{@height} | #{@age} | #{@phone} | #{@email} | #{@nation} | #{avg} "
+  end
 end
 
-student = Students.new("Hieu","1911505310121","Da Nang",0,0,0)
-student.inputPoint
-student.printInfo
 
-student1 = Students.new("Thien","1234567898","Da Nang",0,0,0)
-student1.inputPoint
-student1.printInfo
+array = [Student.new(4,5,8,"Hieu","Black",150,70,18,"0123456789","Hieu12@1234","Kinh"),
+  Student.new(2,5,8,"Thien","Black",170,70,21,"0123456789","thien@12345a","Kinh"),
+  Student.new(10,5,7,"Lam Thien","Grey",159,40,18,"0123456789","lamthien@1234","Kinh"),
+  Student.new(1,9,8,"Tieu Viem","male",155,45,18,"0123456789","tieuviem@1234","Kinh"),
+]
+
+puts "Danh sách chưa sắp xếp : \n",array,"\n"
+
+
+array_sort = array.sort_by { 
+    |el| -el.avg() 
+}
+puts "Danh sách sau khi sắp xếp : \n",array_sort
